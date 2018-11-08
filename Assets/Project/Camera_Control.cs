e57d5ef55cd6e929b235f3355d62abf2287bdeee
Joosep Sisas <joosep.sisas@gmail.com>
2018-11-08 22:24:16 +0000

    Camera working

---
+++ Assets/Project/Camera_Control.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Control : MonoBehaviour
+{
+
+    public float horizontal = 0;
+    public float vertical = 0;
+    public float mouseSensitivity = 1;
+    public GameObject player;
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Mouse X") * mouseSensitivity;
+        vertical -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        vertical = Mathf.Clamp(vertical, -90, 90);
+
+        player.transform.Rotate(0, horizontal, 0);
+        Camera.main.transform.localRotation = Quaternion.Euler(vertical, 0, 0);
+    }
+}
