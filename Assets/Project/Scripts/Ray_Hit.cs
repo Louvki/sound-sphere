1492417862707681b8dc7e5bba8a48430d357101
Joosep Sisas <joosep.sisas@gmail.com>
2018-11-11 08:59:16 +0000

    Crosshair and Raycasting

--- Assets/Project/Scripts/Ray_Hit.cs
+++ Assets/Project/Scripts/Ray_Hit.cs
@@ -12,5 +12,11 @@ public class Ray_Hit : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+		RaycastHit hit;
+
+		if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit)){
+			Debug.Log(hit.transform.name + " was hit!");
+		};
+
 	}
 }
