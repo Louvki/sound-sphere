dec790f36de86777d65516a48650993af7cccbfb
Joosep Sisas <joosep.sisas@gmail.com>
2018-11-21 12:26:15 +0000

    RayHit Refactor

--- Assets/Project/Scripts/RayHit.cs
+++ Assets/Project/Scripts/RayHit.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Ray_Hit : MonoBehaviour {
+public class RayHit : MonoBehaviour {
 
 
     float timer = 0;
